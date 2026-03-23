cc0a8e42e1866df93911e0fdb59a4b125708fe36
mizryl <pham_linda@hotmail.com>
2026-03-23 12:16:31 -0700

    Env Setup

---
+++ dist/sketch.js
@@ -0,0 +1,11 @@
+"use strict";
+// let gameState: "START" | "PLAYING" | "END";
+// gameState = "START";
+function setup() {
+    createCanvas(1280, 704);
+    textAlign(CENTER, CENTER);
+}
+function draw() {
+    background(220);
+}
+//# sourceMappingURL=sketch.js.map
---
+++ sketch.ts
@@ -0,0 +1,13 @@
+
+// let gameState: "START" | "PLAYING" | "END";
+// gameState = "START";
+
+function setup(): void {
+  createCanvas(1280, 704);
+  textAlign(CENTER, CENTER);
+}
+
+function draw(): void {
+  background(220);
+
+}
